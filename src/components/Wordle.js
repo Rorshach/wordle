6eb97b44b18fd46190f09069fadf2c723749c417
Felix Tso <f.tso11200@gmail.com>
2022-05-23 15:56:18 -0700

    Disabled solution and current guess from game page.

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -29,8 +29,6 @@ export default function Wordle({solution}) {
 
     return (
         <div>
-            <div>Solution - { solution }</div>
-            <div>Current guess - { currentGuess }</div>
             <Grid guesses={guesses} currentGuess={currentGuess} turn={turn}/>
             <Keypad usedKeys={usedKeys} />
             {showModal && <Modal isCorrect={isCorrect} turn={turn} solution={solution}/>}
